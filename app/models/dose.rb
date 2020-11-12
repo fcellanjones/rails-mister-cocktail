2004b1ce2fd6c3275446d3080ade226be71279f4
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-12 14:00:02 +0000

    seeded ingredient data and created cocktail controller

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail_id, uniqueness: { scope: :ingredient_id, message: "This igredient already exists!" }
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id, message: "This ingredient already exists!" }
 end
