c3d07ee8499d279331919544d076250f721bff5b
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-12 18:44:10 +0000

    finished the back end for mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,9 +15,15 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose)
+  end
+
   private
 
   def strong_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
